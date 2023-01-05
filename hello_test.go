e7fe768ede6d8cc031f8ceb4d4e0e120e38345bf
John Colton <jcolton96@gmail.com>
2023-01-05 21:12:29 +0000

    Added functionality to hello world function

--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,35 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("John")
+		got := Hello("John", "")
 		want := "Hello, John"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying 'Hello World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jean", "French")
+		want := "Bonjour, Jean"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Bastien", "German")
+		want := "Hallo, Bastien"
 
 		assertCorrectMessage(t, got, want)
 	})
